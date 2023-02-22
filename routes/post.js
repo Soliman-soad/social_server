5baf4a0d60bbad1ee7a6a2cbdf02707138a48a08
Soliman-soad <mdsolimansoad@gmail.com>
2023-02-22 10:50:04 +0600

    post funtionality all done

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post =require("../models/Post");
+const User = require("../models/User");
 const { findById } = require("../models/User");
 
 // create a post
@@ -32,11 +33,62 @@ router.put("/:id", async (req,res)=>{
 
 // delete a post
 
+router.delete("/:id", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.deleteOne();
+            res.status(200).json("Post has been deleted");
+        }else{
+            res.status(403).json("You can delete only your post");
+        }
+    } catch(err){
+        res.status(500).json(err);
+    }
+})
+
 // Like a post 
+router.put("/:id/like",async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push: {likes: req.body.userId}});
+            res.status(200).json("post has been liked");
+        }else{
+            await post.updateOne({$pull : {likes: req.body.userId}});
+            res.status(200).json("post has been disliked")
+        }
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 // get a post 
+router.get("/:id", async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 // get all post
+router.get("/timeline/all", async(req,res)=>{
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts =await Post.find({userId : currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.following.map((friendsId)=>{
+                return Post.find({userId : friendsId});
+            })
+        );
+            res.json(userPosts.concat(...friendPosts));
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
